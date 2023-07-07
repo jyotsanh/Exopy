09401a0989185cc2e1c69ae91588422792f085c6
nothing no will <104152724+Jyotsan-Hamal@users.noreply.github.com>
2023-07-07 10:24:48 +0545

    hjhhh

--- Exopy/urls.py
+++ Exopy/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include("accounts.urls")),
     path("movies/",include("movies.urls")),
-    path("series/",include("series.urls"))
+    path("series/",include("series.urls")),
+    
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,25 @@
 #   ACCOUNT URL
 
 from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_protect
 
 def dash(request):
     if request.method == 'POST':
-        None
+        username = request.POST['username']
+        password1 = request.POST['password']
+        user = authenticate(username=username,password=password1)
+        if user is not None:
+
+            login(request,user)
+            return redirect("movies")
+        else:
+            messages.error(request, "Invalid username or password")
+            return render(request,"dash.html")
+
     else:
         return render(request,"dash.html")
     
@@ -12,9 +27,26 @@ def dash(request):
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        None
+        username = request.POST['username']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        password1 = request.POST['password']
+        password2 = request.POST['confirmpassword']
+        myuser = User.objects.create_user(username,email,password1)
+        myuser.first_name = firstname
+        myuser.last_name = lastname
+
+        myuser.save()
+
+        messages.success(request,"Your Account is succesfully Created")
+
+        return redirect(reverse("accounts:dash")) 
+        
     else:
         return render(request,"register.html")
 
 
     
+def home(request):
+    None
