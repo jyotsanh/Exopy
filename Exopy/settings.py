746f0142ee985ec53de083008005b0a905e17f47
nothing no will <104152724+Jyotsan-Hamal@users.noreply.github.com>
2023-11-29 15:56:25 +0545

    sql database is used

--- Exopy/settings.py
+++ Exopy/settings.py
@@ -83,6 +83,16 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database name',
+        'USER': 'root',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',  # or the hostname where your MySQL server is running
+        'PORT': '3306',      # or the port on which your MySQL server is listening
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
