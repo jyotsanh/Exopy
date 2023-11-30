016a50f078aa0a067eace29adc2bb5d6cce171bc
nothing no will <104152724+Jyotsan-Hamal@users.noreply.github.com>
2023-11-30 17:49:07 +0545

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,10 @@
-echo " BUILD START"
+echo " BUILD START..."
 python3.9 -m pip install -r requirements.txt
+
+echo " Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+
 python3.9 manage.py collectstatic --noinput --clear
-echo " BUILD END" 
+echo " BUILD END..."
