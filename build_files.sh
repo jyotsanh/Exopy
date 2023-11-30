4854d32f7b1fcb98f3a1585014cb3cb6b6f76f61
nothing no will <104152724+Jyotsan-Hamal@users.noreply.github.com>
2023-11-30 18:41:11 +0545

    req

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,6 @@
 echo " BUILD START..."
 python3.9 -m pip install -r requirements.txt
 
-# Set MYSQLCLIENT_CFLAGS and MYSQLCLIENT_LDFLAGS
-export MYSQLCLIENT_CFLAGS=$(mysql_config --cflags)
-export MYSQLCLIENT_LDFLAGS=$(mysql_config --libs)
-
-
 echo " Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
