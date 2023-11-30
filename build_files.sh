533144909e2656dff82c879d2571c77748c6e3c3
nothing no will <104152724+Jyotsan-Hamal@users.noreply.github.com>
2023-11-30 18:02:17 +0545

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,9 @@
 echo " BUILD START..."
 python3.9 -m pip install -r requirements.txt
 
-# Install MySQL Client Dependencies
-sudo apt-get install libmysqlclient-dev
-
 # Set MYSQLCLIENT_CFLAGS and MYSQLCLIENT_LDFLAGS
-export MYSQLCLIENT_CFLAGS=$(pkg-config --cflags mysqlclient)
-export MYSQLCLIENT_LDFLAGS=$(pkg-config --libs mysqlclient)
+export MYSQLCLIENT_CFLAGS=$(mysql_config --cflags)
+export MYSQLCLIENT_LDFLAGS=$(mysql_config --libs)
 
 
 echo " Make Migration..."
