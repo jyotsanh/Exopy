1122ece222ca724a7e9a8a2cbbc3a6f244e76eb9
nothing no will <104152724+Jyotsan-Hamal@users.noreply.github.com>
2023-12-10 16:13:21 +0545

    .env file is generated

--- Exopy/settings.py
+++ Exopy/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -85,12 +87,12 @@ WSGI_APPLICATION = "Exopy.wsgi.app"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'railway',# database name
-        'USER': 'postgres',# user
-        'PASSWORD': '2*aA2*eFec-bB3AfF14B-ad4G*gAb*dD',
-        'HOST': 'monorail.proxy.rlwy.net',  # or the hostname where your MySQL server is running
-        'PORT': '39199',      # or the port on which your MySQL server is listening
+        'ENGINE': os.getenv('ENGINE'),
+        'NAME': os.getenv('NAME'),# database name
+        'USER': os.getenv('USER'),# user
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'), # or the hostname where your MySQL server is running
+        'PORT': os.getenv('PORT'),    # or the port on which your MySQL server is listening
     }
 }
 
--- test.py
+++ test.py
@@ -1,29 +1,14 @@
-import requests
-api_key = "201dadbac9d90432aa44713682a9eb60"
-id = 872585
-movie_url = f'https://api.themoviedb.org/3/movie/{id}?api_key={api_key}'
-movie_response = requests.get(movie_url)
-movie_data = movie_response.json()
-
-
-print("---------------------------------------------------------")
-
-
-def genre(g_id, max_pages=5):
-    api_key = 'YOUR_API_KEY'  # Replace with your actual API key
-    all_data = []
-
-    for page in range(1, max_pages + 1):
-        url = f'https://api.themoviedb.org/3/discover/movie?api_key={api_key}&with_genres={g_id}&page={page}'
-        response = requests.get(url)
-        data = response.json()
-        all_data.extend(data['result'])
-
-    return all_data
-
-
-
-action_data = genre(14)
-print(action_data)
-for movie in action_data['results']:
-    print(movie['title'])
+def findMin(nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        min_num  = nums[0]
+        index = 0
+        for i in range(len(nums)):
+            if nums[i]<min_num:
+                
+                index = i
+                
+        return index
+print(findMin([4,5,6,7,0,1,2]))
