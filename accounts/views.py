5903336064090be2a12d222e2cfae266dabbb9e5
Jyotsan HAMAL <hamaljyotsan@gmail.com>
2024-02-15 18:55:24 +0545

    Log in updates
    new log in updated

--- accounts/views.py
+++ accounts/views.py
@@ -33,19 +33,64 @@ def send_code_to_user(email, code):
 
 def dash(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password1 = request.POST['password']
-        user = authenticate(username=username, password=password1)
+        try:
+            
+            username = request.POST['username']
+            password1 = request.POST['password']
+            firstname = request.POST.get('firstname', False)
+            if firstname:
+                raise ValueError("Invalid input value.")
+            else:
+                user = authenticate(username=username, password=password1)
 
-        if user is not None:
+                if user is not None:
 
-            login(request, user)
-            request.session['username'] = username
-            request.session['password'] = password1
-            return redirect('movies:home')
-        else:
-            messages.error(request, "Invalid username or password")
-            return render(request, 'dash.html')
+                    login(request, user)
+                    request.session['username'] = username
+                    request.session['password'] = password1
+                    return redirect('movies:home')
+                else:
+                    messages.error(request, "Invalid username or password")
+                    return render(request, 'dash.html')
+        except:
+            username = request.POST['username']
+            firstname = request.POST['firstname']
+            lastname = request.POST['lastname']
+            email = request.POST['email']
+            password1 = request.POST['password']
+            password2 = request.POST['confirmpassword']
+
+            if User.objects.filter(username=username):
+                messages.error(request, "username already exist")
+                return redirect('/')
+
+            if User.objects.filter(email=email).exists():
+                messages.error(request, "Email Already Registered!!")
+                return redirect('/')
+
+            if len(username) > 20:
+                messages.error(request, "Username must be under 20 charcters!!")
+                return redirect('/')
+
+            if password1 != password2:
+                messages.error(request, "Passwords didn't matched!!")
+                return redirect('/')
+
+            if not username.isalnum():
+                messages.error(request, "Username must be Alpha-Numeric!!")
+                return redirect('/')
+            
+            myuser = User.objects.create_user(username, email, password1)
+            myuser.first_name = firstname
+            myuser.last_name = lastname
+            try:
+                messages.success(request, "User Successfully saved.")
+                myuser.save()
+            except:
+                messages.error(request, "User is not saved.")
+                return render(request, 'dash.html')
+            
+        return redirect('/')
 
     else:
         request.session.clear()
